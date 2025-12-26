c692ca46e192e7aa4d8e392f9d9f80d6475041f3
juanrey <juanrey@deloitte.com>
2025-12-26 15:30:00 -0300

    proyecto terminado

---
+++ iniciar_notebook.bat
@@ -0,0 +1,10 @@
+@echo off
+echo ========================================
+echo Iniciando Jupyter Notebook
+echo ========================================
+echo.
+echo El notebook se abrira en tu navegador
+echo Presiona Ctrl+C para detener el servidor
+echo.
+jupyter notebook Final_JuanCruzRey.ipynb
+pause
